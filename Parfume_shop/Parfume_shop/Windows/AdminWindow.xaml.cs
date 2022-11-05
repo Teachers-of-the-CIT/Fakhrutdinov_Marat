1e66ca4ddff64bf845e181f0c9d918eddcc36479
marat <fakhrutdinov.m5@gmail.com>
2022-11-05 12:27:52 +0300

    Что-то наделал...

---
+++ Parfume_shop/Parfume_shop/Windows/AdminWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Parfume_shop.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminWindow.xaml
+    /// </summary>
+    public partial class AdminWindow : Window
+    {
+        public AdminWindow()
+        {
+            InitializeComponent();
+            FillProductTable();
+        }
+
+        public void FillProductTable()
+        {
+            try
+            {
+                Parfume_dbEntities1 db = new Parfume_dbEntities1();
+                Products.ItemsSource = db.Product.ToList();
+            }
+            catch
+            {
+                MessageBox.Show("Произошла ошибка связи с базой данных, исправьте ошибку!");
+            }
+        }
+    }
+}
